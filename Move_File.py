58b1ab127a099c4e9be4177a78cdc46d79b283e5
Gargi-1234 <87595595+Gargi-1234@users.noreply.github.com>
2023-05-27 15:44:49 +0530

    Update Move_File.py

--- Move_File.py
+++ Move_File.py
@@ -15,8 +15,8 @@ for file_name in list_of_files :
         continue
     if extension in [".txt", ".doc", ".docx", ".pdf"]:
         path1 = from_dir+'/'+file_name
-        path2 = to_dir+'/'+'image_files'
-        path3 = to_dir+'/'+'image_files'+'/'+file_name
+        path2 = to_dir+'/'+'Document_Files'
+        path3 = to_dir+'/'+'Document_Files'+'/'+file_name
         if os.path.exists(path2):
             print('moving'+file_name+"...")
             shutil.move(path1,path3)
